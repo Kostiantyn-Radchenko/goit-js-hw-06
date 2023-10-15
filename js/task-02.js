d2030c25931e96b311692552f99bdd041f560baf
Konstantyn Radchenko <radchenko1904@gmail.com>
2023-10-15 19:47:31 +0300

    add task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientEl = document.querySelector("#ingredients");
+
+const makeIngredietEl = (items) => {
+  return items.map((el) => {
+    const itemEl = document.createElement("li");
+    itemEl.textContent = el;
+    itemEl.classList.add("item");
+
+    return itemEl;
+  });
+};
+const item = makeIngredietEl(ingredients);
+ingredientEl.append(...item);
